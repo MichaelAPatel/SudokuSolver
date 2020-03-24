b0c316653c3431907de0dcdbc5955ca88ebe4192
Michael <michael.patel.16@cnu.edu>
2020-03-24 13:56:58 -0400

    Added comments and fixed computational times

--- Solver.py
+++ Solver.py
@@ -11,11 +11,10 @@ import matplotlib.pyplot as plt
 puzzle = np.empty((9, 9))
 puzzle1 = np.empty((9, 9))
 puzzle2 = np.empty((9, 9))
-puzzle3 = np.empty((9, 9))
-puzzle4 = np.empty((9, 9))
 exitFlag = False
 
 
+# Thread that runs the systematic recursive solver. Accesses the global exitFlag to know when to terminate.
 class RS(threading.Thread):
 
     def run(self):
@@ -23,25 +22,12 @@ class RS(threading.Thread):
         exitFlag = recursiveSolve()
 
 
-class RSB(threading.Thread):
-
-    def run(self):
-        global exitFlag
-        exitFlag = recursiveSolveBackward()
-
-
+# Thread that runs the human like solver. Accesses the global exitFlag to know when to terminate.
 class RSH(threading.Thread):
 
     def run(self):
         global exitFlag
         exitFlag = recursiveSolveHuman()
-
-
-class RSHB(threading.Thread):
-
-    def run(self):
-        global exitFlag
-        exitFlag = recursiveSolveHumanBackward()
 
 
 # Function determines the values not specified in the given slice of data by preforming an symmetrical difference
@@ -55,6 +41,8 @@ def findValues(slice):
 
 
 # Function checks whether or not the puzzle is solved by checking if any empty (0) cells remain.
+# Arguments
+#   puzz - Puzzle to be checked.
 # Returns
 #   True - If the puzzle is solved.
 #   False - If the puzzle is unsolved.
@@ -62,16 +50,12 @@ def isSolved(puzz):
     return not np.isin(0, puzz)
 
 
-def writeSolution(puzz):
-    global puzzle
-    puzzle = copy.deepcopy(puzz)
-
 # Function to recursively solve the puzzle. First the function checks if the puzzle has already been solved, and if
-# so returns true. If the puzzle has not been solved then the function proceeds to find the first empty cell. Once
-# once the first empty cell is identified then it determines all the possible values that that individual cell could
-# have. The function then loops through those values recursively calling itself. If the recursion gets stuck then the
-# function resets the puzzle to its previous states and attempts the next guess.
-def recursiveSolve(order='forward'):
+# so returns true or exits the thread. If the puzzle has not been solved then the function proceeds to find the first
+# empty cell. Once the first empty cell is identified then it determines all the possible values that that individual
+# cell could have. The function then loops through those values recursively calling itself. If the recursion gets stuck
+# then the function resets the puzzle to its previous states and attempts the next guess.
+def recursiveSolve():
     # The puzzle must be defined globally here so that this function can both access it AND make changes to it as
     # by default python functions can access variables outside their scope but they are immutable.
     global puzzle1
@@ -103,56 +87,29 @@ def recursiveSolve(order='forward'):
             puzzle1 = copy.deepcopy(prePuzzle)
     return False
 
-def recursiveSolveBackward():
-    # The puzzle must be defined globally here so that this function can both access it AND make changes to it as
-    # by default python functions can access variables outside their scope but they are immutable.
-    global puzzle3
+
+# Function to recursively solve the puzzle. First the function checks if the puzzle has already been solved, and if
+# so returns true or exits the thread. If the puzzle has not been solved, then the function proceeds to loop through
+# the puzzle to determine how many valid guesses there are for each cell. It then picks the cell with the minimum
+# number of guesses. The function then loops through those guess recursively calling itself. If the recursion gets stuck
+# then the function resets the puzzle to its previous states and attempts the next guess.
+def recursiveSolveHuman():
+    global puzzle2
     global exitFlag
 
     if exitFlag:
         sys.exit()
 
-    if isSolved(puzzle3):
-        return True
-
-    emptyCells = np.where(puzzle3 == 0)
-    firstCell = list(zip(emptyCells[0], emptyCells[1]))[0]
-    i, j = firstCell
-    idxrow = i / 3 * 3
-    idxcol = j / 3 * 3
-
-    guesses = list(reduce(np.intersect1d, (findValues(puzzle3[i, :]), findValues(puzzle3[:, j]), findValues(puzzle3[idxrow:idxrow + 3, idxcol:idxcol + 3]))))
-    guesses.reverse()
-    if len(guesses) == 0:
-        return False
-
-    for guess in guesses:
-        prePuzzle = copy.deepcopy(puzzle3)
-        puzzle3[i][j] = guess
-        result = recursiveSolveBackward()
-        if result:
-            return True
-        else:
-            puzzle3 = copy.deepcopy(prePuzzle)
-    return False
-
-
-def recursiveSolveHuman():
-    # The puzzle must be defined globally here so that this function can both access it AND make changes to it as
-    # by default python functions can access variables outside their scope but they are immutable.
-    global puzzle2
-    global exitFlag
-
     if isSolved(puzzle2):
         return True
 
     guessesPerCell = []
-    for i in range(0,9):
-        for j in range(0,9):
+    for i in range(0, 9):
+        for j in range(0, 9):
             idxrow = i / 3 * 3
             idxcol = j / 3 * 3
             if puzzle2[i][j] == 0:
-                guessesPerCell.append(len(list(reduce(np.intersect1d, (findValues(puzzle2[i, :]), findValues(puzzle2[:, j]),findValues(puzzle2[idxrow:idxrow + 3, idxcol:idxcol + 3]))))))
+                guessesPerCell.append(len(list(reduce(np.intersect1d, (findValues(puzzle2[i, :]), findValues(puzzle2[:, j]), findValues(puzzle2[idxrow:idxrow + 3, idxcol:idxcol + 3]))))))
             else:
                 guessesPerCell.append(100)
 
@@ -175,49 +132,9 @@ def recursiveSolveHuman():
     return False
 
 
-def recursiveSolveHumanBackward():
-    # The puzzle must be defined globally here so that this function can both access it AND make changes to it as
-    # by default python functions can access variables outside their scope but they are immutable.
-    global puzzle4
-    global exitFlag
-
-    if isSolved(puzzle4):
-        return True
-
-    guessesPerCell = []
-    for i in range(0, 9):
-        for j in range(0, 9):
-            idxrow = i / 3 * 3
-            idxcol = j / 3 * 3
-            if puzzle4[i][j] == 0:
-                guessesPerCell.append(len(list(reduce(np.intersect1d, (
-                findValues(puzzle4[i, :]), findValues(puzzle4[:, j]),
-                findValues(puzzle4[idxrow:idxrow + 3, idxcol:idxcol + 3]))))))
-            else:
-                guessesPerCell.append(100)
-
-    i = guessesPerCell.index(min(guessesPerCell)) / 9
-    j = guessesPerCell.index(min(guessesPerCell)) % 9
-    idxrow = i / 3 * 3
-    idxcol = j / 3 * 3
-    guesses = list(reduce(np.intersect1d, (findValues(puzzle4[i, :]), findValues(puzzle4[:, j]), findValues(puzzle4[idxrow:idxrow + 3, idxcol:idxcol + 3]))))
-    guesses.reverse()
-    if len(guesses) == 0:
-        return False
-
-    for guess in guesses:
-        prePuzzle = copy.deepcopy(puzzle4)
-        puzzle4[i][j] = guess
-        result = recursiveSolveHumanBackward()
-        if result:
-            return True
-        else:
-            puzzle4 = copy.deepcopy(prePuzzle)
-    return False
-
 solutionTimes = []
 puzzleFiles = os.listdir('Puzzles/')
-solvers = [0, 0, 0, 0]
+solvers = [0, 0]
 
 # Solve each puzzle in the Puzzle directory.
 for puzzleFile in puzzleFiles:
@@ -229,24 +146,20 @@ for puzzleFile in puzzleFiles:
             puzzleArray.append(row)
 
     puzzle = np.array(puzzleArray)
+    # Make copies of the puzzles so that the threads do not try to edit the same puzzle.
     puzzle1 = copy.deepcopy(puzzle)
     puzzle2 = copy.deepcopy(puzzle)
-    puzzle3 = copy.deepcopy(puzzle)
-    puzzle4 = copy.deepcopy(puzzle)
     threadRS = RS()
     threadRSH = RSH()
-    threadRSB = RSB()
-    threadRSHB = RSHB()
     exitFlag = False
     start = time.time()
-    threadRS.start()
+    # Start the race.
     threadRSH.start()
-    threadRSB.start()
-    threadRSHB.start()
-    threadRS.join()
-    threadRSH.join()
-    threadRSB.join()
-    threadRSHB.join()
+    threadRS.start()
+    # Wait until both threads exit. Presumably one with the solution and the other with an exception since the puzzle
+    # was solved by the other thread.
+    while threading.active_count() > 1:
+        continue
     processtime = time.time() - start
     solutionTimes.append(processtime)
     solver = ''
@@ -258,14 +171,6 @@ for puzzleFile in puzzleFiles:
         puzzle = copy.deepcopy(puzzle2)
         solver = '\nHuman Recursive'
         solvers[1] += 1
-    elif isSolved(puzzle3):
-        puzzle = copy.deepcopy(puzzle3)
-        solver = '\nPure Recursive Backward'
-        solvers[3] += 1
-    elif isSolved(puzzle4):
-        puzzle = copy.deepcopy(puzzle4)
-        solver = '\nHuman Recursive Backward'
-        solvers[4] += 1
 
     with open('Solutions/' + puzzleFile.replace('.txt', '_SOLUTION.txt'), 'w') as f:
         if not isSolved(puzzle):
@@ -276,11 +181,24 @@ for puzzleFile in puzzleFiles:
         f.write('\nDuration: ' + str(processtime))
         f.write(solver)
 
-# Print some basic statistics and plot the results as a histogram.
+# Print some basic statistics and plot the results as bar graphs.
 print 'Puzzle ' + str(solutionTimes.index(max(solutionTimes)) + 1) + ' took the longest to solve, ' + str(max(solutionTimes)) + ' seconds.'
 print 'Puzzle ' + str(solutionTimes.index(min(solutionTimes)) + 1) + ' took the shortest to solve, ' + str(min(solutionTimes)) + ' seconds.'
 print list(solvers)
 
 solutionTimes = np.array([round(i) for i in solutionTimes])
-plt.hist(solutionTimes)
+fig, ax = plt.subplots(1, 2, sharey=True, tight_layout=True)
+
+labels, counts = np.unique(solutionTimes, return_counts=True)
+ax[0].bar(labels, counts, align='center', color='plum')
+ax[1].set_xticks(labels)
+ax[0].set_xlabel('Computation Time (seconds)')
+ax[0].set_ylabel('Frequency')
+
+objects = ('Recursive Solve', 'Human Recursive\n Solve')
+y_pos = np.arange(len(objects))
+
+ax[1].bar(y_pos, solvers, align='center', color='plum')
+ax[1].set_xticks((0, 1))
+ax[1].set_xticklabels(objects)
 plt.show()
